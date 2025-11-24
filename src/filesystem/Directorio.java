5b99f066ab082bec8cbe8e7249f6721329ff1a42
adolfobarberi1205 <adolfo.barberi@correo.unimet.edu.ve>
2025-11-24 16:44:13 -0400

    Primer commit poco  codigo

--- src/filesystem/Directorio.java
+++ src/filesystem/Directorio.java
@@ -8,6 +8,61 @@ package filesystem;
  *
  * @author user
  */
-public class Directorio {
-    
+public class Directorio extends NodoFS {
+
+    public static final int MAX_HIJOS = 100;
+
+    private NodoFS[] hijos;
+    private int numHijos;
+
+    public Directorio(String nombre, Directorio padre) {
+        super(nombre, padre);
+        this.hijos = new NodoFS[MAX_HIJOS];
+        this.numHijos = 0;
+    }
+
+    public boolean agregarHijo(NodoFS hijo) {
+        if (numHijos >= MAX_HIJOS) {
+            return false; // lleno
+        }
+        hijos[numHijos] = hijo;
+        numHijos++;
+        return true;
+    }
+
+    public void eliminarHijo(NodoFS hijo) {
+        for (int i = 0; i < numHijos; i++) {
+            if (hijos[i] == hijo) {
+                hijos[i] = hijos[numHijos - 1];
+                hijos[numHijos - 1] = null;
+                numHijos--;
+                return;
+            }
+        }
+    }
+
+    public NodoFS getHijo(int indice) {
+        if (indice < 0 || indice >= numHijos) {
+            return null;
+        }
+        return hijos[indice];
+    }
+
+    public int getNumHijos() {
+        return numHijos;
+    }
+
+    public NodoFS buscarPorNombre(String nombreBuscado) {
+        for (int i = 0; i < numHijos; i++) {
+            if (hijos[i] != null && hijos[i].getNombre().equals(nombreBuscado)) {
+                return hijos[i];
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean esArchivo() {
+        return false;
+    }
 }
--- src/filesystem/NodoFS.java
+++ src/filesystem/NodoFS.java
@@ -8,6 +8,39 @@ package filesystem;
  *
  * @author user
  */
-public class NodoFS {
-    
+public abstract class NodoFS {
+
+    protected String nombre;
+    protected Directorio padre;
+
+    public NodoFS(String nombre, Directorio padre) {
+        this.nombre = nombre;
+        this.padre = padre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Directorio getPadre() {
+        return padre;
+    }
+
+    public void setPadre(Directorio padre) {
+        this.padre = padre;
+    }
+
+    // Ruta tipo /root/dir/subdir/archivo
+    public String getRutaCompleta() {
+        if (padre == null) {
+            return "/" + nombre;
+        }
+        return padre.getRutaCompleta() + "/" + nombre;
+    }
+
+    public abstract boolean esArchivo();
 }
